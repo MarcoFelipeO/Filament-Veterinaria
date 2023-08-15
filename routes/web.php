16242ea6852ca6f554cfe0d6f074fe8a72991d4a
Marko <mafeorellana09@gmail.com>
2023-08-15 17:54:08 -0400

    probando filament

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+use Illuminate\Support\Facades\DB;
+
+Route::get('/mysql', function () {
+    try {
+        DB::connection('mysql')->getPdo();
+        return 'Conexión Mysql exitosa';
+    } catch (\Exception $e) {
+        return 'Error al conectar con MySql: ' . $e->getMessage();
+    }
+});
