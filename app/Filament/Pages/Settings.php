bfa9c087f5b5866dc4f53315e53d8d56de59db88
Marko <mafeorellana09@gmail.com>
2023-08-18 10:29:53 -0400

    creacion de Creación de un widget de gráfico

--- app/Filament/Pages/Settings.php
+++ app/Filament/Pages/Settings.php
@@ -4,16 +4,28 @@ namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
 
+use Illuminate\Contracts\Support\Htmlable;
+
+
 class Settings extends Page
 {
 
-    protected static ?string $title = 'Index Du Pruebiña';
+ 
+    public function getTitle(): string | Htmlable
+    {
+        return __('Custom Page Title');   //nombre en la pagina 
+    }
+
+    protected ?string $subheading = 'Subtitulo de prueba';
+
+    protected static ?string $navigationLabel = 'Apartado de Prueba';  //Nombre de pagina en lateral izquierdo
+    
 
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.settings';
 
 
-
+    
 
 }
--- resources/views/filament/pages/settings.blade.php
+++ resources/views/filament/pages/settings.blade.php
@@ -1,10 +1,36 @@
 <x-filament-panels::page>
 
 <div>
-    <div style="text-RIGHT: center">
-        hELLO GUYS
 
+
+  
+    
+    <div class="flex p-4">
+        <div class="w-1/4 bg-gray-200 p-4">
+            <h2 class="text-lg font-semibold mb-4">Categorías</h2>
+            <ul class="list-disc pl-4">
+                <li class="mb-2"><a href="#" class="text-blue-500 hover:underline">Ropa</a></li>
+                <li class="mb-2"><a href="#" class="text-blue-500 hover:underline">Electrónica</a></li>
+                <li class="mb-2"><a href="#" class="text-blue-500 hover:underline">Hogar</a></li>
+                <li class="mb-2"><a href="#" class="text-blue-500 hover:underline">Accesorios</a></li>
+            </ul>
+        </div>
+    
+        <div class="w-3/4 p-4">
+            <h1 class="text-2xl font-bold mb-4">Productos Destacados</h1>
+            <!-- Contenido de los productos destacados aquí --> 
+           
+        </div>
     </div>
+    
+    <div class="bg-gray-200 py-4 text-center">
+        <p class="text-gray-600">Explora nuestras categorías para encontrar lo que necesitas.</p>
+    </div>
+    
+
 </div>
+
+
+
 
 </x-filament-panels::page>
